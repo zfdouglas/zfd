739026ec6b202937b5f78ac686d3baec5235513c
zfdouglas <zacharyfdouglas@gmail.com>
2023-02-23 10:55:57 -0500

    feat:  make browser router work with gh apges

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -11,7 +11,7 @@ import { Blog } from "../Screens/Blog";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <NavBar />
       </div>
