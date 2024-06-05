c962805289940ca6c80b9cc4a96ed9801d6531b8
zfdouglas <zacharyfdouglas@gmail.com>
2024-06-04 20:52:38 -0400

    feat: about about

--- src/Copy/About.ts
+++ src/Copy/About.ts
@@ -6,9 +6,11 @@ export interface AboutType {
 export const About: AboutType = {
   name: "> ZACK DOUGLAS",
   blurb:
-    "Hey there,  <br/><br/> My name is Zack and I am a fullstack developer that has a special love for getting my hands dirty in the front end. When I am not working, you can find me hiking, searching out the nearest coffee shop, or conceptualizing the next passion project. <br/><br/> Feel free to connect with me either via email, linkedin, or github. <br/><br/> -ZFD",
+    "Hey there,  <br/><br/> My name is Zack and I am a senior fullstack developer that has a special love for getting my hands dirty in the front end. When I am not working, you can find me hiking, searching out the nearest coffee shop, or conceptualizing the next passion project. <br/><br/> Feel free to connect with me either via email, linkedin, or github. <br/><br/> -ZFD",
   skills: [
     "Typescript",
+    "C#",
+    ".NET",
     "Java",
     "React",
     "HTML/CSS",
@@ -16,6 +18,7 @@ export const About: AboutType = {
     "Python",
     "UI Design",
     "Node.js",
+    "Postgres",
     "MongoDB",
     "Python",
     "Adobe Illustrator",
